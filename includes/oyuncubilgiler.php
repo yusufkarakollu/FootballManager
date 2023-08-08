c99063aae6354ce3f98b926646901e2cbdf9e705
yusufkarakollu <karakolluyusuf34@gmail.com>
2023-08-08 12:45:18 +0300

    fixed: oyuncubilgiler.php

--- includes/oyuncubilgiler.php
+++ includes/oyuncubilgiler.php
@@ -1,33 +1,33 @@
 <?php
 include "./head.php"
 ?>
-<div class="col-md-10 offset-1 mt-3">
-  <div class="card">
-    <div class="row">
-      <div class="col-md-2">
-
-        <img src="./8198-1685035469.webp" class="card-img-top" alt="Yüklenemedi">
-
-        <div class="card-body">
-          <h5 class="card-title">Cristiano Ronaldo</h5>
+<br>
+<div class="container">
+  <div class="row">
+    <div class="col-md-2">
+      <a href="" class="text-decoration-none">
+        <div class="card">
+          <img src="./img/885.png" alt="">
+          <h5 class="text-dark">Cristiano Ronaldo</h5>
         </div>
-      </div>
-      <div class="col-md-10">
-        <div class="row">
-          <div class="col-md-5">
-            <div class="border rounded">
-              <h3 class="text-info ms-3">Player Info</h3>
-              <div class="card-title text-info ms-5">
-                <p>05/02/1985</p>
-                <p>Portugal</p>
-                <p>1,87</p>
-              </div>
+      </a>
+    </div>
+    <div class="col-md-6">
+      <div class="card">
+        <div class="border rounded-top d-flex">
+          <div class="col-md-6 border">
+            <h3 class="text-info ms-3">Player Info</h3>
+            <div class="card-title fw-bold text-info ms-5">
+              <p>05/02/1985</p>
+              <p>Portugal</p>
+              <p>1,87</p>
             </div>
           </div>
-          <div class="col-md-7">
+          <div class="col-md-6">
             <h3 class="text-info ms-3">Contract</h3>
-            <div class="card-title text-info ms-5">
-              <img src="./img/al-nassr-fc-logo-7FBA561439-seeklogo.com.png" style="width: 25px;" title="al nasr">
+            <div class="card-title fw-bold text-info ms-5">
+              <img src="./img/al-nassr-fc-logo-7FBA561439-seeklogo.com.png" style="width: 25px;">
+              <p>Al Nassr</p>
               <p>Wage: 200 million €</p>
               <p>End Of Contract: 30/06/2025</p>
             </div>
@@ -36,14 +36,21 @@ include "./head.php"
       </div>
     </div>
   </div>
-  <br>
-  <div class="card">
+</div>
+<br>
+<div class="col-md-6 offset-3">
+  <div class="card ms-5 shadow p-3 mb-5 bg-body rounded">
     <div class="col-md-10">
       <div class="row">
         <div class="col-md-3 ms-4">
-            <acronym class="ms-2" title="How well a player is at taking corners">Corners</acronym>
-            <hr>
-            <acronym class="ms-2" title="How well a player can run with the ball in his feet">Dribbling</acronym>
+          <acronym class="ms-2" title="How well a player is at taking corners">Corners</acronym>
+          <hr>
+          <acronym class="ms-2" title="How well a player can run with the ball in his feet">Dribbling</acronym>
+        </div>
+        <div class="col-md-3 ms-4">
+          <acronym class="ms-2" title="How well a player is at taking corners">Corners</acronym>
+          <hr>
+          <acronym class="ms-2" title="How well a player can run with the ball in his feet">Dribbling</acronym>
         </div>
       </div>
     </div>
