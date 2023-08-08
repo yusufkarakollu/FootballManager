71e2c8cfbca0213d453fc01af7b150d125cc2484
yusufkarakollu <karakolluyusuf34@gmail.com>
2023-08-08 23:34:58 +0300

    fixed: oyuncu ozellikleri duzenlendi

--- includes/oyuncubilgiler.php
+++ includes/oyuncubilgiler.php
@@ -38,28 +38,211 @@ include "./head.php"
   </div>
 </div>
 <br>
-<div class="col-md-6 offset-3">
+<!-- <div class="col-md-6 offset-3">
   <div class="card ms-5 shadow p-3 mb-5 bg-body rounded">
-    <div class="col-md-10">
-      <div class="row">
+  <div class="row">
+    <div class="col-md-12">
         <div class="col-md-4 ms-4">
-            <acronym class="ms-2" title="How well a player is at taking corners">Corners</acronym>
-            <div class="stat-value-0-10">
-              <p>8</p>
-            </div>
+          
+        </div>
+        <div class="col-md-4 ms-4">
+          
+        </div>
+        <div class="col-md-4 ms-4">
+          
+
+        </div>
+      </div>
+    </div>
+  </div>
+</div> -->
+
+<div class="col-6 offset-3">
+  <div class="card ms-5 shadow p-3 mb-5 bg-body rounded">
+    <div class="col-12">
+      <div class="row">
+        <div class="col-4">
+          <p class="p-ozellik">Corners</p>
+          <div class="stat-value-0-10">
+            <p>10</p>
+          </div>
           <hr>
-          <acronym class="ms-2" title="How well a player can run with the ball in his feet">Dribbling</acronym>
+          <p class="p-ozellik">Crossing</p>
+          <div class="stat-value-11-15">
+            <p>11</p>
+          </div>
+          <hr>
+          <p class="p-ozellik">Dribbling</p>
+          <div class="stat-value-11-15">
+            <p>11</p>
+          </div>
+          <hr>
+          <p class="p-ozellik">Finishing</p>
+          <div class="stat-value-16-20">
+            <p>19</p>
+          </div>
+          <hr>
+          <p class="p-ozellik">First Touch</p>
           <div class="stat-value-11-15">
             <p>15</p>
           </div>
+          <hr>
+          <p class="p-ozellik">Free Kick Taking</p>
+          <div class="stat-value-0-10">
+            <p>10</p>
+          </div>
+          <hr>
+          <p class="p-ozellik">Heading</p>
+          <div class="stat-value-16-20">
+            <p>19</p>
+          </div>
+          <hr>
+          <p class="p-ozellik">Long Shots</p>
+          <div class="stat-value-11-15">
+            <p>15</p>
+          </div>
+          <hr>
+          <p class="p-ozellik">Long Throws</p>
+          <div class="stat-value-0-10">
+            <p>5</p>
+          </div>
+          <hr>
+          <p class="p-ozellik">Marking</p>
+          <div class="stat-value-0-10">
+            <p>3</p>
+          </div>
+          <hr>
+          <p class="p-ozellik">Passing</p>
+          <div class="stat-value-11-15">
+            <p>13</p>
+          </div>
+          <hr>
+          <p class="p-ozellik">Penalty Taking</p>
+          <div class="stat-value-16-20">
+            <p>19</p>
+          </div>
         </div>
-        <div class="col-md-4 ms-4">
-          <acronym class="ms-2" title="How well a player is at taking corners">Corners</acronym>
+        <div class="col-4">
+          <p class="p-ozellik">Tackling</p>
+          <div class="stat-value-0-10">
+            <p>2</p>
+          </div>
+          <hr>
+          <p class="p-ozellik">Technique</p>
+          <div class="stat-value-11-15">
+            <p>15</p>
+          </div>
+          <hr>
+          <p class="p-ozellik">Aggression</p>
+          <div class="stat-value-0-10">
+            <p>6</p>
+          </div>
+          <hr>
+          <p class="p-ozellik">Anticipation</p>
+          <div class="stat-value-16-20">
+            <p>18</p>
+          </div>
+          <hr>
+          <p class="p-ozellik">Bravery</p>
+          <div class="stat-value-11-15">
+            <p>12</p>
+          </div>
+          <hr>
+          <p class="p-ozellik">Composure</p>
+          <div class="stat-value-11-15">
+            <p>15</p>
+          </div>
+          <hr>
+          <p class="p-ozellik">Concentration</p>
+          <div class="stat-value-11-15">
+            <p>14</p>
+          </div>
+          <hr>
+          <p class="p-ozellik">Decisions</p>
+          <div class="stat-value-11-15">
+            <p>12</p>
+          </div>
+          <hr>
+          <p class="p-ozellik">Determination</p>
           <div class="stat-value-16-20">
             <p>20</p>
           </div>
           <hr>
-          <acronym class="ms-2" title="How well a player can run with the ball in his feet">Dribbling</acronym>
+          <p class="p-ozellik">Flair</p>
+          <div class="stat-value-16-20">
+            <p>17</p>
+          </div>
+          <hr>
+          <p class="p-ozellik">Leadership</p>
+          <div class="stat-value-16-20">
+            <p>18</p>
+          </div>
+          <hr>
+          <p class="p-ozellik">Off The Ball</p>
+          <div class="stat-value-16-20">
+            <p>17</p>
+          </div>
+        </div>
+        <div class="col-4">
+          <p class="p-ozellik">Positioning</p>
+          <div class="stat-value-0-10">
+            <p>2</p>
+          </div>
+          <hr>
+          <p class="p-ozellik">Teamwork</p>
+          <div class="stat-value-0-10">
+            <p>5</p>
+          </div>
+          <hr>
+          <p class="p-ozellik">Vision</p>
+          <div class="stat-value-11-15">
+            <p>11</p>
+          </div>
+          <hr>
+          <p class="p-ozellik">Work Rate</p>
+          <div class="stat-value-0-10">
+            <p>7</p>
+          </div>
+          <hr>
+          <p class="p-ozellik">Acceleration</p>
+          <div class="stat-value-11-15">
+            <p>12</p>
+          </div>
+          <hr>
+          <p class="p-ozellik">Agility</p>
+          <div class="stat-value-0-10">
+            <p>10</p>
+          </div>
+          <hr>
+          <p class="p-ozellik">Balance</p>
+          <div class="stat-value-11-15">
+            <p>13</p>
+          </div>
+          <hr>
+          <p class="p-ozellik">Jumping Reach</p>
+          <div class="stat-value-16-20">
+            <p>17</p>
+          </div>
+          <hr>
+          <p class="p-ozellik">Natural Fitness</p>
+          <div class="stat-value-16-20">
+            <p>16</p>
+          </div>
+          <hr>
+          <p class="p-ozellik">Pace</p>
+          <div class="stat-value-11-15">
+            <p>13</p>
+          </div>
+          <hr>
+          <p class="p-ozellik">Stamina</p>
+          <div class="stat-value-11-15">
+            <p>13</p>
+          </div>
+          <hr>
+          <p class="p-ozellik">Strength</p>
+          <div class="stat-value-11-15">
+            <p>15</p>
+          </div>
         </div>
       </div>
     </div>
